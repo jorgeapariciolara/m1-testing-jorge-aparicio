108425c95227c7c2d27f97b7d958f22d27ef31dd
Jorge <jorgeaparicio79@gmail.com>
2021-10-08 13:13:42 +0200

    Initial commit

---
+++ src/main/java/com/example/m1testingjorgeaparicio/M1TestingJorgeAparicioApplication.java
@@ -0,0 +1,13 @@
+package com.example.m1testingjorgeaparicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M1TestingJorgeAparicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M1TestingJorgeAparicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/m1testingjorgeaparicio/M1TestingJorgeAparicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.m1testingjorgeaparicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1TestingJorgeAparicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
